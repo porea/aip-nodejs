ca8772d87b5d4473f82f19f67fea277267af6a7c
Ming Zhao <mingzhao@Mings-MacBook-Air.local>
2017-08-14 20:13:09 +1000

    added express-validator rules (back-end)

--- week4-preparation/app.js
+++ week4-preparation/app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var bodyParser = require("body-parser"); 
+var expressValidator = require('express-validator');
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));  
+app.use(expressValidator());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -12,15 +14,37 @@ app.use(function(req, res, next) {
 var title,date,duration,genre,synopsis;
 
 app.post('/submit', function (req, res) {
-	console.log(req.body);
+	req.checkBody('title', 'Invalid title').notEmpty();
+	req.checkBody('date', 'Invalid date').notEmpty();
+	req.checkBody('duration', 'Invalid duration').notEmpty();
+	req.checkBody('genre', 'Invalid genre').notEmpty();
+	req.checkBody('synopsis', 'Invalid synopsis').notEmpty();
 
-    title = req.body.title;
-    date = req.body.date;
-    duration = req.body.duration;
-    genre = req.body.genre;
-    synopsis = req.body.synopsis;
-    
-	res.json(req.body);
+	var errors = req.validationErrors();
+	if (errors) {
+		console.log("found errors")
+	}else{
+		req.sanitizeBody('title').escape();
+		req.sanitizeBody('title').trim();
+		req.sanitizeBody('date').escape();
+		req.sanitizeBody('date').trim();
+		req.sanitizeBody('duration').escape();
+		req.sanitizeBody('duration').trim();
+		req.sanitizeBody('genre').escape();
+		req.sanitizeBody('genre').trim();
+		req.sanitizeBody('synopsis').escape();
+		req.sanitizeBody('synopsis').trim();
+
+		title = req.body.title;
+	    date = req.body.date;
+	    duration = req.body.duration;
+	    genre = req.body.genre;
+	    synopsis = req.body.synopsis;
+
+	    console.log(req.body);
+
+	    res.json(req.body);
+	}
 })
 
 app.get('/query', function (req, res) {
